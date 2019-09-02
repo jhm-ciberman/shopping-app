27083ec91b859e5a6d93a3e32b6eb1db43bd2a12
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2019-09-02 04:48:49 -0300

    Add basic authentication and app layout

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
+Auth::routes();
 
 Route::resource('products', 'ProductController')->only(['index', 'show']);
